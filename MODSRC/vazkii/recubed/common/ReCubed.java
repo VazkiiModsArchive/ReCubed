15dce5591de0da9a8656e2f3b484298e460d0725
Vasco Lavos <vasco.men@hotmail.com>
2013-12-13 14:32:14 +0000

    Initial code!

---
+++ MODSRC/vazkii/recubed/common/ReCubed.java
@@ -0,0 +1,19 @@
+/**
+ * This class was created by <Vazkii>. It's distributed as
+ * part of the ReCubed Mod.
+ * 
+ * ReCubed is Open Source and distributed under a
+ * Creative Commons Attribution-NonCommercial-ShareAlike 3.0 License
+ * (http://creativecommons.org/licenses/by-nc-sa/3.0/deed.en_GB)
+ * 
+ * File Created @ [Dec 13, 2013, 2:29:53 PM (GMT)]
+ */
+package vazkii.recubed.common;
+
+import vazkii.recubed.common.lib.LibMisc;
+import cpw.mods.fml.common.Mod;
+
+@Mod(modid = LibMisc.MOD_ID, name = LibMisc.MOD_NAME, version = LibMisc.MOD_VERSION)
+public class ReCubed {
+
+}
---
+++ MODSRC/vazkii/recubed/common/lib/LibMisc.java
@@ -0,0 +1,19 @@
+/**
+ * This class was created by <Vazkii>. It's distributed as
+ * part of the ReCubed Mod.
+ * 
+ * ReCubed is Open Source and distributed under a
+ * Creative Commons Attribution-NonCommercial-ShareAlike 3.0 License
+ * (http://creativecommons.org/licenses/by-nc-sa/3.0/deed.en_GB)
+ * 
+ * File Created @ [Dec 13, 2013, 2:30:06 PM (GMT)]
+ */
+package vazkii.recubed.common.lib;
+
+public final class LibMisc {
+
+	public static final String MOD_ID = "ReCubed";
+	public static final String MOD_NAME = "ReCubed";
+	public static final String MOD_VERSION = "ANT:VERSION-ANT:BUILD";
+	
+}
